edb072b4260132bf0970136e8c32c172755452dc
hike.327 <hayk.sahakyan@sada.com>
2023-07-26 16:58:37 +0400

    lower

--- 2-projects/project.tf
+++ 2-projects/project.tf
@@ -1,14 +1,14 @@
 module "project" {
   source          = "./modules/project"
-  project_name    = var.project_name
+  project_name    = "${lower(var.project_name)}"
   org_id          = local.org_id
   folder_id       = local.folder["${var.env}"]
   billing_account = local.billing_account
   activate_apis   = var.activate_apis
-  requestor       = var.requestor
-  team            = var.team
-  supervisor      = var.supervisor
-  environment     = var.env
+  requestor       = "${lower(var.requestor)}"
+  team            = "${lower(var.team)}"
+  supervisor      = "${lower(var.supervisor)}"
+  environment     = "${lower(var.env)}"
   workspace       = var.workspace
   bigdata         = var.bigdata
   compute         = var.compute
